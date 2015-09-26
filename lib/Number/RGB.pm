b8fc61150993ad27ea8578fc1364f9df21e70fe6
Zoffix Znet <cpan@zoffix.com>
2015-09-26 17:04:09 -0400

    Bunch of stuff and tests

--- lib/Number/RGB.pm
+++ lib/Number/RGB.pm
@@ -6,55 +6,57 @@ use warnings;
 # VERSION
 
 use vars qw[$CONSTRUCTOR_SPEC];
-use Carp;
 use Scalar::Util qw[looks_like_number];
 use Params::Validate qw[:all];
 use base qw[Class::Accessor::Fast];
 use Attribute::Handlers 0.99;
+use Carp;
+our @CARP_NOT = ('Attribute::Handlers', __PACKAGE__);
+$Carp::Internal{'attributes'}++; # no idea why doesn't work in @CARP_NOT
 
 sub import {
-	my $class  = shift;
-	my $caller = (caller)[0];
-	eval qq[
-		package $caller;
-		use Attribute::Handlers;
-		sub RGB :ATTR(SCALAR) { goto &$class\::RGB }
-		package $class;
-	];
+    my $class  = shift;
+    my $caller = (caller)[0];
+    eval qq[
+        package $caller;
+        use Attribute::Handlers;
+        sub RGB :ATTR(RAWDATA) { goto &$class\::RGB }
+        package $class;
+    ];
 }
 
 use overload fallback => 1,
-	'""'  => \&as_string,
-	'+'   => sub { shift->_op_math('+',  @_) },
-	'-'   => sub { shift->_op_math('-',  @_) },
-	'*'   => sub { shift->_op_math('*',  @_) },
-	'/'   => sub { shift->_op_math('/',  @_) },
-	'%'   => sub { shift->_op_math('%',  @_) },
-	'**'  => sub { shift->_op_math('**', @_) },
-	'<<'  => sub { shift->_op_math('<<', @_) },
-	'>>'  => sub { shift->_op_math('>>', @_) },
-	'&'   => sub { shift->_op_math('&',  @_) },
-	'^'   => sub { shift->_op_math('^',  @_) },
-	'|'   => sub { shift->_op_math('|',  @_) };
+    '""'  => \&as_string,
+    '+'   => sub { shift->_op_math('+',  @_) },
+    '-'   => sub { shift->_op_math('-',  @_) },
+    '*'   => sub { shift->_op_math('*',  @_) },
+    '/'   => sub { shift->_op_math('/',  @_) },
+    '%'   => sub { shift->_op_math('%',  @_) },
+    '**'  => sub { shift->_op_math('**', @_) },
+    '<<'  => sub { shift->_op_math('<<', @_) },
+    '>>'  => sub { shift->_op_math('>>', @_) },
+    '&'   => sub { shift->_op_math('&',  @_) },
+    '^'   => sub { shift->_op_math('^',  @_) },
+    '|'   => sub { shift->_op_math('|',  @_) };
 
 sub new {
-	my $class = shift;
-	my %params = validate( @_,  $CONSTRUCTOR_SPEC );
-	croak "$class->new() requires parameters" unless keys %params;
+    my $class = shift;
+    my %params = validate( @_,  $CONSTRUCTOR_SPEC );
+    croak "$class->new() requires parameters" unless keys %params;
 
-	my %rgb;
-	if ( defined $params{rgb} ) {
-		@rgb{qw[r g b]} = @{$params{rgb}};
-	} elsif ( defined $params{rgb_number} ) {
-		return $class->new(rgb => [($params{rgb_number})x3]);
-	} elsif ( defined $params{hex} ) {
-		my $hex = $params{hex};
-		$hex =~ s/^#//;
-		$hex =~ s/(.)/$1$1/g if length($hex) == 3;
-		@rgb{qw[r g b]} = map hex, $hex =~ /(.{2})/g;
-	}
+    my %rgb;
+    if ( defined $params{rgb} ) {
+        @rgb{qw[r g b]} = @{$params{rgb}};
+    } elsif ( defined $params{rgb_number} ) {
+        return $class->new(rgb => [($params{rgb_number})x3]);
+    } elsif ( defined $params{hex} ) {
+        my $hex = $params{hex};
+        $hex =~ s/^#//;
+        $hex =~ s/(.)/$1$1/g if length($hex) == 3;
+        @rgb{qw[r g b]} = map hex, $hex =~ /(.{2})/g;
+    }
 
-	$class->SUPER::new(\%rgb);
+    $class->SUPER::new(\%rgb);
 }
 
 __PACKAGE__->mk_accessors( qw[r g b] );
@@ -63,35 +65,39 @@ sub rgb       { [ map $_[0]->$_, qw[r g b] ] }
 sub hex       { '#' . join '', map { substr sprintf('0%x',$_[0]->$_), -2 } qw[r g b] }
 sub hex_uc    { uc shift->hex }
 sub as_string {
-	join ',', map $_[0]->$_, qw[r g b]
+    join ',', map $_[0]->$_, qw[r g b]
 }
 
 sub _op_math {
-	my ($self,$op, $other, $reversed) = @_;
-	ref($self)->new(rgb => [
-		map {
-			my $x = $self->$_;
-			my $y = ref($other) && overload::Overloaded($other) ? $other->$_ : $other;
-			int eval ($reversed ? "$y $op $x" : "$x $op $y");
-		} qw[r g b]
-	] );
+    my ($self,$op, $other, $reversed) = @_;
+    ref($self)->new(rgb => [
+        map {
+            my $x = $self->$_;
+            my $y = ref($other) && overload::Overloaded($other) ? $other->$_ : $other;
+            my $ans = eval ($reversed ? "$y $op $x" : "$x $op $y");
+            $ans = sprintf '%.0f', $ans||0;
+            $ans = 0 if $ans < 0; $ans = 255 if $ans > 255;
+            $ans;
+        } qw[r g b]
+    ] );
 }
 
 sub new_from_guess {
-	my ($class, $value) = @_;
-
+    my ($class, $value) = @_;
+    $value = [ $value =~ /\d+/g ] if $value =~ /,/;
     my $is_single_rgb = looks_like_number($value) && $value>=0 && $value<=255;
-	foreach my $param ( keys %{$CONSTRUCTOR_SPEC} ) {
+
+    foreach my $param ( keys %{$CONSTRUCTOR_SPEC} ) {
         next if $param eq 'hex' and $is_single_rgb;
-		my $self = eval { $class->new($param => $value) };
-		return $self if defined $self;
-	}
-	croak "$class->new_from_guess() couldn't guess type for ($value)";
+        my $self = eval { $class->new($param => $value) };
+        return $self if defined $self;
+    }
+    croak q{couldn't guess value type};
 }
 
-sub RGB :ATTR(SCALAR) {
-	my ($var, $data) = @_[2,4];
-	$$var = __PACKAGE__->new_from_guess(@$data);
+sub RGB :ATTR(RAWDATA) {
+    my ($var, $data) = @_[2,4];
+    $$var = __PACKAGE__->new_from_guess($data);
 }
 
 $CONSTRUCTOR_SPEC = {
@@ -99,25 +105,27 @@ $CONSTRUCTOR_SPEC = {
         type      => ARRAYREF,
         optional  => 1,
         callbacks => {
-        	'three elements'    => sub { 3 == @{$_[0]} },
-        	'only digits'       => sub { 0 == grep /\D/, @{$_[0]} },
-        	'between 0 and 255' => sub { 3 == grep { $_ >= 0 && $_ <= 255 } @{$_[0]} },
+            'three elements'    => sub { 3 == @{$_[0]} },
+            'only digits'       => sub { 0 == grep /\D/, @{$_[0]} },
+            'between 0 and 255' => sub { 3 == grep { $_ >= 0 && $_ <= 255 } @{$_[0]} },
         },
     },
     rgb_number => {
-    	type      => SCALAR,
-    	optional  => 1,
-    	callbacks => {
-    		'only digits'       => sub { $_[0] !~ /\D/ },
-    		'between 0 and 255' => sub { $_[0] >= 0 && $_[0] <= 255 },
-    	},
+        type      => SCALAR,
+        optional  => 1,
+        callbacks => {
+            'only digits'       => sub { $_[0] !~ /\D/ },
+            'between 0 and 255' => sub {
+                looks_like_number($_[0]) and $_[0] >= 0 && $_[0] <= 255
+            },
+        },
     },
     hex => {
-    	type      => SCALAR,
-    	optional  => 1,
-    	callbacks => {
-    		'hex format' => sub { $_[0] =~ /^#?(?:[\da-f]{3}|[\da-f]{6})$/i },
-    	},
+        type      => SCALAR,
+        optional  => 1,
+        callbacks => {
+            'hex format' => sub { $_[0] =~ /^#?(?:[\da-f]{3}|[\da-f]{6})$/i },
+        },
     }
 };
 
@@ -137,7 +145,7 @@ Number::RGB - Manipulate RGB Tuples
   my $white :RGB(255);
   my $black :RGB(0);
 
-  my $gray = $black + (($white - $black) / 2);
+  my $gray = $black + ( $white / 2 );
 
   my @rgb = @{ $white->rgb };
   my $hex = $black->hex;
@@ -162,6 +170,7 @@ make construction shorter.
 
   my $red   = Number::RGB->new(rgb => [255,0,0])
   my $blue  = Number::RGB->new(hex => '#0000FF');
+  my $blue  = Number::RGB->new(hex => '#00F');
   my $black = Number::RGB->new(rgb_number => 0);
 
 This constructor accepts named parameters. One of three parameters are
@@ -174,7 +183,8 @@ I<blue>.
 C<hex> is a hexidecimal representation of an RGB tuple commonly used in
 Cascading Style Sheets. The format begins with an optional hash (C<#>)
 and follows with three groups of hexidecimal numbers represending
-I<red>, I<green>, and I<blue> in that order.
+I<red>, I<green>, and I<blue> in that order. A shorthand, 3-digit version
+can be used: C<#123> is equivalent to C<#112233>.
 
 C<rgb_number> is a single integer to use for each of the three primary colors.
 This is shorthand to create I<white>, I<black>, and all shades of
@@ -207,7 +217,7 @@ Accessor and mutator for the I<blue> value.
 
 =head3 C<rgb>
 
-Returns a list reference containing three elements. In order they
+Returns a array reference containing three elements. In order they
 represent I<red>, I<green>, and I<blue>.
 
 =head3 C<hex>
@@ -235,6 +245,40 @@ would be the string C<255,255,255>.
 
 This attribute is exported to the caller and provides a shorthand wrapper
 around L</new_from_guess>.
+
+=head2 Overloads
+
+C<Number::RGB> L<overloads|overload> the following operations:
+
+    ""
+    +
+    -
+    *
+    /
+    %
+    **
+    <<
+    >>
+    &
+    ^
+    |
+
+Stringifying a C<Number::RGB> object will produce a string with three
+RGB tuples joined with commas. All other operators operate on each
+individual RGB tuple number.
+
+If the tuple value is below C<0> after
+the operation, it will set to C<0>. If the tuple value is above C<255> after
+the operation, it will set to C<255>.
+
+I<Note:> illegal operations (such us dividing by zero) result in the tuple
+value being set to C<0>.
+
+Operations create new C<Number::RGB> objects,
+which means that even something as strange as this still works:
+
+    my $color :RGB(5,10,50);
+    print 110 - $color; # prints '105,100,60'
 
 =for pod_spiffy hr
 
